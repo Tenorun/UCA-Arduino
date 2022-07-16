596828c4bf9b9b2c076320cf0e336061e4fd4483
Tenorun <ngdtv123@gmail.com>
2022-07-16 18:50:42 +0900

    밝기에 따라 껐다 켜지는 LED

--- CDS/CDS.ino
+++ CDS/CDS.ino
@@ -1,6 +1,15 @@
 void setup() {
+  Serial.begin(9600);
   
 }
 void loop() {
+  int lightValue = analogRead(A0);
+  Serial.println(lightValue);
 
+  if(lightValue<200){
+    digitalWrite(7,HIGH);
+  }else{
+    digitalWrite(7,LOW);
+  }
+  
 }
