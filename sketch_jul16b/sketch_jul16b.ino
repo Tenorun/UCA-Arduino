fab4bb61212d71d471d9256df0847887c6e6f7c9
Tenorun <ngdtv123@gmail.com>
2022-07-16 18:36:07 +0900

    조도 센서

--- sketch_jul16b/sketch_jul16b.ino
+++ sketch_jul16b/sketch_jul16b.ino
@@ -1,12 +1,10 @@
 void setup() {
-  // put your setup code here, to run once:
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(7, LOW);
   digitalWrite(5, HIGH);
   delay(100);
