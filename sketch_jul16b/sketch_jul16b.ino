f40c2cebb54ab295ecab8680df69a03f1208f82c
Tenorun <ngdtv123@gmail.com>
2022-07-16 17:35:25 +0900

    LED Wavein'

--- sketch_jul16b/sketch_jul16b.ino
+++ sketch_jul16b/sketch_jul16b.ino
@@ -1,13 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(7, HIGH);
-  delay(500);
-  
   digitalWrite(7, LOW);
-  delay(500);
+  digitalWrite(5, HIGH);
+  delay(100);
+  
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  delay(100);
+
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  delay(100);
 }
