d69df52cee53c10ed87b0510445675e0ed350228
Tenorun <ngdtv123@gmail.com>
2022-07-23 18:12:14 +0900

    토글 버튼

--- Press_the_button/Press_the_button.ino
+++ Press_the_button/Press_the_button.ino
@@ -1,7 +1,29 @@
+int flag = 0;
+boolean toggle_state = 0;
+
 void setup() {
-  
+  Serial.begin(9600);
+  pinMode(4,INPUT_PULLUP);
+  pinMode(7,OUTPUT);
 }
 
 void loop() {
+  if(digitalRead(4) == 0){
+    if(flag == 0){
+      flag = 1;
+      toggle_state = !toggle_state;
+    }
+  }
+  else{
+    if(flag == 1){
+      flag = 0;
+    }
+  }
 
+  if(toggle_state){
+    digitalWrite(7,HIGH);
+  }else{
+    digitalWrite(7,LOW);
+  }
+  
 }
